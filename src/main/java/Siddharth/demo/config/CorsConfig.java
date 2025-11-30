ecb3346c13550c112ee1e53c7051c3e4514f23f5
josephojo818 <cobrasiddharthgawaigmail@gmail.com>
2025-11-30 12:32:07 +0545

    Allowed frontend request to flow

--- src/main/java/Siddharth/demo/config/CorsConfig.java
+++ src/main/java/Siddharth/demo/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "https://josephojo818.github.io/GothamFrontend/"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
